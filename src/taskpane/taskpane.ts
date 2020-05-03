9f33c8c5f0147ed8622eb392f5bc9dafc2ab6eb3
J. Garcia <janikgar@gmail.com>
2020-05-03 12:23:16 -0700

    update deps; formatting

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -13,7 +13,14 @@ class Rule {
   institution: string;
   amount: string;
 
-  constructor(category: string, tagRaw: string, description: string, account: string, institution: string, amount: string) {
+  constructor(
+    category: string,
+    tagRaw: string,
+    description: string,
+    account: string,
+    institution: string,
+    amount: string
+  ) {
     this.category = category;
     this.tag = new Set(tagRaw.split(","));
     this.tag.delete(""); // The request from Excel will parse in an empty cell as an empty string; we will remove this by default
@@ -24,28 +31,28 @@ class Rule {
   }
 
   matchDescription(testString: string) {
-    return this.description === "" || testString.includes(this.description)
+    return this.description === "" || testString.includes(this.description);
   }
   matchAccount(testString: string) {
-    return this.account === "" || testString.includes(this.account)
+    return this.account === "" || testString.includes(this.account);
   }
   matchInstitution(testString: string) {
-    return this.institution === "" || testString.includes(this.institution)
+    return this.institution === "" || testString.includes(this.institution);
   }
   matchAmount(testNumber: number) {
     let lowerBound = /(?:>([-\d.]+))/.exec(this.amount) || null; // The whole capture group, including "<" or ">", will match on the first returned item; below we will use lowerBound[1] to receive the capture group with just the number portion
     let upperBound = /(?:<([-\d.]+))/.exec(this.amount) || null;
     if (!lowerBound && !upperBound) {
-      return true
+      return true;
     }
     if (lowerBound && upperBound) {
-      return testNumber > parseFloat(lowerBound[1]) && testNumber < parseFloat(upperBound[1])
+      return testNumber > parseFloat(lowerBound[1]) && testNumber < parseFloat(upperBound[1]);
     }
     if (lowerBound) {
-      return testNumber > parseFloat(lowerBound[1])
+      return testNumber > parseFloat(lowerBound[1]);
     }
     if (upperBound) {
-      return testNumber < parseFloat(upperBound[1])
+      return testNumber < parseFloat(upperBound[1]);
     }
   }
 }
@@ -66,13 +73,13 @@ class Transaction {
   checkNumber: string;
   transactionId: string;
   categorized: boolean;
-  constructor (rowArray: Array<string>, address: number = null) {
+  constructor(rowArray: Array<string>, address: number = null) {
     this.date = new Date(rowArray[0]);
     this.description = rowArray[1];
     this.category = rowArray[2];
     this.tags = new Set(rowArray[3].split(","));
     this.tags.delete(""); // The request from Excel will return an empty cell as an empty string; removing this by default
-    this.amount = parseFloat(rowArray[4].replace("$",""));
+    this.amount = parseFloat(rowArray[4].replace("$", ""));
     this.account = rowArray[5];
     this.accountNum = rowArray[6];
     this.institution = rowArray[7];
@@ -106,10 +113,10 @@ function makeRules(values: string[][]): Array<Rule> {
   let outputData: Array<Rule> = [];
   values.shift(); // slide off the first row, which is column headers
   for (let row of values) {
-    let outputRow = new Rule(row[0], row[1], row[2], row[3], row[4], row[5])
-    outputData.push(outputRow)
+    let outputRow = new Rule(row[0], row[1], row[2], row[3], row[4], row[5]);
+    outputData.push(outputRow);
   }
-  return outputData
+  return outputData;
 }
 
 async function getAutoTagData() {
@@ -118,12 +125,11 @@ async function getAutoTagData() {
     const sheet = context.workbook.worksheets.getItem("AutoTag").getUsedRange(true);
     sheet.load();
 
-    await context.sync()
-      .then(() => {
-        rules = makeRules(sheet.text);
-      })
-    })
-  return rules
+    await context.sync().then(() => {
+      rules = makeRules(sheet.text);
+    });
+  });
+  return rules;
 }
 
 async function getTransactions(): Promise<Map<string, Transaction>> {
@@ -133,78 +139,80 @@ async function getTransactions(): Promise<Map<string, Transaction>> {
     let sheet = context.workbook.worksheets.getItem("Transactions").getUsedRange(true);
     sheet.load("columnCount, rowCount");
 
-    await context.sync()
+    await context
+      .sync()
       .then(() => {
-        txnShape = {"columnCount": sheet.columnCount, "rowCount": sheet.rowCount}
+        txnShape = { columnCount: sheet.columnCount, rowCount: sheet.rowCount };
       })
       .catch(err => {
-        return err
-      })
-  })
+        return err;
+      });
+  });
 
   let txnPaginator: Map<string, Transaction> = new Map<string, Transaction>();
   let columnsToSearch: Object = {};
   await Excel.run(async context => {
-    let sheet = context.workbook.worksheets.getItem("Transactions")
-      .getRangeByIndexes(0, 0, 1, txnShape["columnCount"])
+    let sheet = context.workbook.worksheets.getItem("Transactions").getRangeByIndexes(0, 0, 1, txnShape["columnCount"]);
     sheet.load();
-    await context.sync()
+    await context
+      .sync()
       .then(() => {
         for (let i = 0; i < sheet.values[0].length; i++) {
           let column = sheet.values[0][i];
           if (columnsMatched.includes(column)) {
-            columnsToSearch[i] = column
+            columnsToSearch[i] = column;
           }
         }
       })
       .then(async () => {
         let step = 1000; // Number of transactions processed per batch
-        for (let i = 1; i < txnShape["rowCount"]; i+= step) {
-          let sheet = context.workbook.worksheets.getItem("Transactions")
-            .getRangeByIndexes(i, 0, step, txnShape["columnCount"]).getUsedRangeOrNullObject(true);
+        for (let i = 1; i < txnShape["rowCount"]; i += step) {
+          let sheet = context.workbook.worksheets
+            .getItem("Transactions")
+            .getRangeByIndexes(i, 0, step, txnShape["columnCount"])
+            .getUsedRangeOrNullObject(true);
           sheet.load();
-    
-          await context.sync()
+
+          await context
+            .sync()
             .then(() => {
               for (let j = 0; j < sheet.text.length; j++) {
-                let row = sheet.text[j]
-                let rowAddress = i + j + 1 // i = current step position; j = current row within this round-trip; +1 because of the header row
+                let row = sheet.text[j];
+                let rowAddress = i + j + 1; // i = current step position; j = current row within this round-trip; +1 because of the header row
                 let newRow = new Transaction(row, rowAddress);
                 txnPaginator.set(newRow.transactionId, newRow);
               }
             })
             .catch(err => {
-              logLocal(err.toString())
-            })
+              logLocal(err.toString());
+            });
         }
-      })
-    })
-  return txnPaginator
+      });
+  });
+  return txnPaginator;
 }
 
-async function runRules(rules: Array<Rule>, transactionObject: Map<string, Transaction>, uncategorizedOnly: boolean = false): Promise<Array<Transaction>> {
+async function runRules(
+  rules: Array<Rule>,
+  transactionObject: Map<string, Transaction>,
+  uncategorizedOnly: boolean = false
+): Promise<Array<Transaction>> {
   let matches: Array<any> = [];
   let transactions = Array.from(transactionObject.values());
-  logLocal(`Running with ${rules.length} rules and ${transactions.length} transactions`)
-  logLocal(`Only running uncategorized transactions?: ${uncategorizedOnly}`)
+  logLocal(`Running with ${rules.length} rules and ${transactions.length} transactions`);
+  logLocal(`Only running uncategorized transactions?: ${uncategorizedOnly}`);
   for (let transaction of transactions) {
     for (let rule of rules) {
-      let ruleMatch = (
+      let ruleMatch =
         rule.matchDescription(transaction.description) &&
         rule.matchAccount(transaction.account) &&
         rule.matchInstitution(transaction.institution) &&
-        rule.matchAmount(transaction.amount)
-      )
-      let categorizable = (
+        rule.matchAmount(transaction.amount);
+      let categorizable =
         !transaction.categorized && // If already categorized earlier, don't re-categorize
-        (
-          !uncategorizedOnly || // If not already categorized and we're running all transactions 
-          (
-            uncategorizedOnly && // If not already categorized, we're only running uncategorized transactions, and the category is indeed empty
-            transaction.category === ""
-          )
-        )
-      )
+        (!uncategorizedOnly || // If not already categorized and we're running all transactions
+          (uncategorizedOnly && // If not already categorized, we're only running uncategorized transactions, and the category is indeed empty
+            transaction.category === ""));
       if (ruleMatch) {
         if (categorizable) {
           transaction.category = rule.category;
@@ -217,7 +225,7 @@ async function runRules(rules: Array<Rule>, transactionObject: Map<string, Trans
     }
     matches.push(transaction);
   }
-  return matches
+  return matches;
 }
 
 async function getTagColumns(): Promise<any> {
@@ -226,18 +234,18 @@ async function getTagColumns(): Promise<any> {
     let searchRange = sheet.getRange();
     let tagSearch = searchRange.findOrNullObject("Tags", {
       completeMatch: true,
-      matchCase: true,
-    })
+      matchCase: true
+    });
     tagSearch.load("columnindex");
     return context.sync().then(() => {
       if (tagSearch === null) {
-        return
+        return;
       }
       let tagColumn = String.fromCharCode(tagSearch.columnIndex + 65);
-      return tagColumn
-    })
-  })
-  return returnPromise
+      return tagColumn;
+    });
+  });
+  return returnPromise;
 }
 
 async function getCatColumns(): Promise<any> {
@@ -246,18 +254,18 @@ async function getCatColumns(): Promise<any> {
     let searchRange = sheet.getRange();
     let catSearch = searchRange.findOrNullObject("Category", {
       completeMatch: true,
-      matchCase: true,
-    })
+      matchCase: true
+    });
     catSearch.load("columnindex");
     return context.sync().then(() => {
       if (catSearch === null) {
-        return
+        return;
       }
       let catColumn = String.fromCharCode(catSearch.columnIndex + 65);
-      return catColumn
-    })
-  })
-  return returnPromise
+      return catColumn;
+    });
+  });
+  return returnPromise;
 }
 
 async function getTagLastRow(): Promise<any> {
@@ -266,12 +274,11 @@ async function getTagLastRow(): Promise<any> {
     let usedRange = sheet.getUsedRange(true);
     let usedRangeRow = usedRange.getLastRow();
     usedRangeRow.load("rowIndex");
-    return context.sync()
-      .then(() => {
-        return usedRangeRow.rowIndex + 1;
-      })
-  })
-  return returnPromise
+    return context.sync().then(() => {
+      return usedRangeRow.rowIndex + 1;
+    });
+  });
+  return returnPromise;
 }
 
 function scrapeTags(txns: Array<Transaction>): string[][] {
@@ -279,11 +286,11 @@ function scrapeTags(txns: Array<Transaction>): string[][] {
   for (let txn of txns) {
     let tagArray: string;
     if (txn.tags.size > 0) {
-      tagArray = [...txn.tags].join(",")
+      tagArray = [...txn.tags].join(",");
     }
     tagList.push([tagArray]);
   }
-  return tagList
+  return tagList;
 }
 
 function scrapeCats(txns: Array<Transaction>): string[][] {
@@ -291,7 +298,7 @@ function scrapeCats(txns: Array<Transaction>): string[][] {
   for (let txn of txns) {
     catList.push([txn.category]);
   }
-  return catList
+  return catList;
 }
 
 async function rewriteCells(address: string, column: string[][]) {
@@ -299,8 +306,8 @@ async function rewriteCells(address: string, column: string[][]) {
     let sheet = context.workbook.worksheets.getItem("Transactions");
     let rewriteRange = sheet.getRange(address);
     rewriteRange.values = column;
-    return context.sync()
-  })
+    return context.sync();
+  });
 }
 
 function clearLog() {
@@ -317,18 +324,18 @@ async function runCat() {
   await getCatColumns()
     .then(columnResult => {
       catColumn = columnResult;
-      return getTagLastRow()
+      return getTagLastRow();
     })
     .then(rowResult => {
       catLastRow = rowResult;
-      return getAutoTagData()
+      return getAutoTagData();
     })
     .then(result => {
       categoryData = result;
-      return getTransactions()
+      return getTransactions();
     })
     .then(txnResult => {
-      rewriteAddress = `Transactions!${catColumn}2:${catColumn}${catLastRow}`
+      rewriteAddress = `Transactions!${catColumn}2:${catColumn}${catLastRow}`;
       return runRules(categoryData, txnResult, false);
     })
     .then(ruleResult => {
@@ -336,7 +343,10 @@ async function runCat() {
     })
     .then(catResult => {
       return rewriteCells(rewriteAddress, catResult);
-    }).catch(err => {logLocal(err);})
+    })
+    .catch(err => {
+      logLocal(err);
+    });
 }
 
 async function runTag() {
@@ -347,19 +357,19 @@ async function runTag() {
   clearLog();
   await getTagColumns()
     .then(columnResult => {
-      tagColumn = columnResult
-      return getTagLastRow()
+      tagColumn = columnResult;
+      return getTagLastRow();
     })
     .then(rowResult => {
       tagLastRow = rowResult;
-      return getAutoTagData()
+      return getAutoTagData();
     })
     .then(result => {
       autoTagData = result;
-      return getTransactions()
+      return getTransactions();
     })
     .then(txnResult => {
-      rewriteAddress = `Transactions!${tagColumn}2:${tagColumn}${tagLastRow}`
+      rewriteAddress = `Transactions!${tagColumn}2:${tagColumn}${tagLastRow}`;
       return runRules(autoTagData, txnResult, false);
     })
     .then(ruleResult => {
@@ -368,5 +378,7 @@ async function runTag() {
     .then(tagResult => {
       rewriteCells(rewriteAddress, tagResult);
     })
-    .catch(err => { logLocal(err);});
+    .catch(err => {
+      logLocal(err);
+    });
 }
